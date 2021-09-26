1aa3ca46b96fcb4f1c22017946544facaa8b4532
Jacob <jacob@jacobolofsson.se>
2021-09-26 14:24:23 +0200

    feat: add install script outline

---
+++ install.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import logging
+import pathlib
+import sys
+
+from git.repo import Repo
+
+_logger = logging.getLogger(__file__)
+
+
+def _create_symlink(src: pathlib.Path, dest: pathlib.Path) -> None:
+    _logger.debug(f"Linking {src} -> {dest}")
+
+
+def check_version() -> None:
+    _logger.info("Checking if this repo is up to date")
+
+
+def install_packages() -> None:
+    _logger.info("Installing packages")
+
+
+def link_config_files() -> None:
+    _logger.info("Symlinking config files")
+
+
+def validate_setup() -> None:
+    _logger.info("Validating setup")
+
+
+def main() -> int:
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s %(name)s: [%(levelname)s] %(message)s",
+        datefmt="%Y-%m-%d %T",
+    )
+
+    check_version()
+    install_packages()
+    link_config_files()
+
+    _logger.info("Setup complete")
+
+    validate_setup()
+    return 0
+
+
+if __name__ == "__main__":
+    try:
+        sys.exit(main())
+    except Exception as ex:
+        _logger.critical(f"Uncaught exception: {ex}")
+        _logger.debug("Stack trace:", exc_info=True)
+
+    sys.exit(-1)
